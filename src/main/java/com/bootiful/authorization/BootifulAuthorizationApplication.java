c97deb38f81b5da3d821edc9715bd1cef5303044
Hai Nguyen <haind.vng@gmail.com>
2022-01-08 12:08:43 +0700

    init project

---
+++ src/main/java/com/bootiful/authorization/BootifulAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.bootiful.authorization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootifulAuthorizationApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BootifulAuthorizationApplication.class, args);
+  }
+
+}
